14ebe77097806f2c75a0b056113fd8b5c57e9c4e
SarahGeraldo <sah.nicole16@gmail.com>
2025-05-09 08:15:15 -0300

    dOntem

--- MOBILEapp/App.js
+++ MOBILEapp/App.js
@@ -1,53 +1,45 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { TextInput } from "react-native-web";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Image,
+  ImageBackground,
+  // Button,
+  TouchableOpacity, // forma de capiturar o click com maior estilização
+} from "react-native";
 
 export default function App() {
+  const ChangeText = () => {
+    console.log("HELLO");
+  };
   return (
-    <View style={styles.container}>
-      {/* EXEMPLO UM: */}
-
-      {/* {[GRUPO, {LINHA}]}: */}
-      {/* <View style={(styles.ex1, { backgroundColor: "pink" })}></View> */}
-
-      {/* ----------------------------------------------------------- */}
-      {/* {GRUPO}: */}
-      {/* <Text style={styles.ex2}>HELLO</Text> */}
-
-      {/* ----------------------------------------------------------- */}
-      {/* {{LINHA}}: */}
-      {/* <Text style={{ color: "green", fontSize: 33 }}>BYE</Text> */}
-
-      {/* ----------------------------------------------------------- */}
-      {/* {{MULTIPLOS GRUPOS}}: */}
-      {/* <View style={[styles.ex4, styles.card]}></View> */}
-
-      {/* ----------------------------------------------------------- */}
-      {/* EXEMPLO DOIS: */}
-
-      {/* <View style={styles.container}>
-        <View style={styles.card}>
-          <text>Card1</text>
-        </View>
-
-        <View style={styles.card}>
-          <text>Card2</text>
-        </View>
-
-        <View style={styles.card}>
-          <text>Card3</text>
-        </View>
-      </View>
-      <StatusBar style="auto" /> */}
-
-      {/* ----------------------------------------------------------- */}
-      {/* EXEMPLO TRÊS: */}
-
-      <View style={styles.container}>
-        <TextInput style={styles.input1} />
-        <TextInput style={styles.input2} />
-      </View>
-    </View>
+    <ImageBackground
+      style={styles.container}
+      source={require("./src/assets/img/PA2.jpg")}
+      resizeMode="cover" // adaptação da imagem na tela
+    >
+      <Text style={styles.text}>Good </Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Digite seu nome"
+        secureTextEntry={true}
+        placeholderTextColor="white"
+        inputMode={"numeric"}
+        autoCapitalize="characters"
+        multiline={true}
+        onChangeText={ChangeText}
+      />
+      {/* <Button
+        title="click me!"
+        color="#ff0000"
+        onPress={() => alert("You click on button")}
+      /> */}
+      <TouchableOpacity style={styles.btn}>
+        <Text style={styles.txtBtn}>click me! </Text>
+      </TouchableOpacity>
+    </ImageBackground>
   );
 }
 
@@ -57,6 +49,36 @@ const styles = StyleSheet.create({
     backgroundColor: "rgb(107, 115, 53)",
     alignItems: "center",
     justifyContent: "center",
+  },
+  text: {
+    fontSize: 50,
+    color: "#000",
+  },
+  input: {
+    color: "rgb(236, 87, 170)",
+    width: 300,
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "fff",
+  },
+  image: {
+    width: "50%",
+    height: "20%",
+    borderRadius: 80,
+  },
+  btn: {
+    width: 100,
+    padding: 10,
+    backgroundColor: "transparent",
+    color: "rgb(236, 87, 170)",
+    alignItems: "center",
+    marginTop: 10,
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: "rgb(236, 87, 170)",
+  },
+  txtBtn: {
+    color: "rgb(236, 87, 170)",
   },
 
   /* ex2: {
@@ -74,6 +96,8 @@ const styles = StyleSheet.create({
     backgroundColor: "yellow",
   }, */
 
+  /* EXEMPLO QUATRO: 
+  
   input1: {
     borderWidth: 0,
     borderBottomWidth: 1,
@@ -93,5 +117,131 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 2, height: 2 },
     shadowOpacity: 0.3,
     shadowRadius: 2,
-  },
+  }, */
 });
+
+{
+  /* <View style={styles.container}>*/
+}
+{
+  /* EXEMPLO UM: */
+}
+
+{
+  /* {[GRUPO, {LINHA}]}: */
+}
+{
+  /* <View style={(styles.ex1, { backgroundColor: "pink" })}></View> */
+}
+
+{
+  /* ----------------------------------------------------------- */
+}
+{
+  /* {GRUPO}: */
+}
+{
+  /* <Text style={styles.ex2}>HELLO</Text> */
+}
+
+{
+  /* ----------------------------------------------------------- */
+}
+{
+  /* {{LINHA}}: */
+}
+{
+  /* <Text style={{ color: "green", fontSize: 33 }}>BYE</Text> */
+}
+
+{
+  /* ----------------------------------------------------------- */
+}
+{
+  /* {{MULTIPLOS GRUPOS}}: */
+}
+{
+  /* <View style={[styles.ex4, styles.card]}></View> */
+}
+
+{
+  /* ----------------------------------------------------------- */
+}
+{
+  /* EXEMPLO DOIS: */
+}
+
+{
+  /* <View style={styles.container}>
+        <View style={styles.card}>
+          <text>Card1</text>
+        </View>
+
+        <View style={styles.card}>
+          <text>Card2</text>
+        </View>
+
+        <View style={styles.card}>
+          <text>Card3</text>
+        </View>
+      </View>
+      <StatusBar style="auto" /> */
+}
+
+{
+  /* ----------------------------------------------------------- */
+}
+{
+  /* EXEMPLO TRÊS: */
+}
+
+{
+  /* <View style={styles.container}>
+        <TextInput style={styles.input1} />
+        <TextInput style={styles.input2} />
+      </View>
+    </View> */
+}
+
+{
+  /* ----------------------------------------------------------- */
+}
+{
+  /* EXEMPLO QUATRO: */
+}
+
+/*     <View style={[styles.container, { backgroundColor: "rgb(107, 115, 53)" }]}>
+      <Text style={styles.text}>Good </Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Digite seu nome"
+        secureTextEntry={true}
+        placeholderTextColor="white"
+        inputMode={"numeric"}
+        autoCapitalize="characters"
+        multiline={true}
+        onChangeText={ChangeText}
+      />
+
+      <Image
+        source={require("./src/assets/img/PA2.jpg")}
+        style={styles.image}
+      />
+    </View>
+    */
+
+// -----------------------------------------
+
+/*
+      btn: {
+    width: 100,
+    padding: 10,
+    backgroundColor: "transparent",
+    color: "rgb(236, 87, 170)",
+    alignItems: "center",
+    marginTop: 10,
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: "rgb(236, 87, 170)",
+  },
+  */

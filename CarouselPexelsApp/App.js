2f5c8fed05a8d9e74f713cf12b755e83588fa339
SarahGeraldo <sah.nicole16@gmail.com>
2025-08-15 15:00:17 -0300

    FimDaExplicacao

---
+++ CarouselPexelsApp/App.js
@@ -0,0 +1,140 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Image, Dimensions, FlatList } from 'react-native';
+// import reanimated
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  useAnimatedScrollHandler,
+  interpolate
+} from 'react-native-reanimated';
+// import axios
+import axios from 'axios';
+
+const { width } = Dimensions.get("screen");
+const imageWidth = width * 0.7;
+const imageHeight = imageWidth * 1.76;
+const spacing = 12;
+
+function Photo({ item, index, scrollX }) {
+  const stylez = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { scale: interpolate(scrollX.value, [index - 1, index, index + 1], [1.6, 1, 1.6]) },
+        { rotate: `${interpolate(scrollX.value, [index - 1, index, index + 1], [15, 1, -15])}deg` }
+      ]
+    }
+  })
+  return (
+    <View style={{ width: imageWidth, height: imageHeight, overflow: 'hidden', borderRadius: 20 }}>
+      <Animated.Image source={{ uri: item.src.large }} style={[{ flex: 1 }, stylez]} />
+    </View>
+  )
+}
+
+function BackdropPhoto({ photo, index, scrollX }) {
+  const stylez = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(scrollX.value, [index - 1, index, index + 1], [0, 1, 0])
+    }
+  })
+  return (
+    <Animated.Image source={{ uri: photo.src.large }} style={[StyleSheet.absoluteFillObject, stylez]} />
+  )
+}
+
+export default function App() {
+  const [data, setData] = useState({ photos: [] });
+
+  /* useSharedValue => Reativo as animações, quando o nosso scrollX.value for alterado
+  todas as animações serão alteradas. */
+  const scrollX = useSharedValue(0);
+
+  /* useAnimatedScrollHandler => Hook do reanimated, que serve para "escutar"
+  o evento de rolagem (onScroll) da nossa lista */
+  const onScroll = useAnimatedScrollHandler((e) => {
+    // e.contentOffset.x => Distância em pixels que a lista já foi rolada na horizontal
+    // scrollX.value = 320 / (300 + 20)
+    // scrollX.value = 0
+    // scrollX.value = 1
+    // scrollX.value = 2
+    scrollX.value = e.contentOffset.x / (imageWidth + spacing);
+    // "=" é sinal de atribuição
+  })
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  const fetchData = async () => {
+    try {
+      const res = await axios.get(
+        `https://api.pexels.com/v1/search?query=${encodeURIComponent('mobile wallpaper')}&orientation=portrait`,
+        {
+          headers: { Authorization: 'O8yMufVQH8GklntL1DHSbKgzWICnfOz37XgYgXL0tzlXUGAErYr3BLr7' }
+        });
+      setData(res.data);
+      console.log(res.data)
+    } catch (error) {
+      console.log("Erro ao buscar as imagens: ", error)
+    }
+  }
+
+  if (data.photos.length === 0) {
+    return (
+      <View style={styles.container}>
+        <Text>Carregando ...</Text>
+      </View>
+    )
+  }
+
+  return (
+    <View style={styles.container}>
+      {/* "absoluteFillObject" resume => position: absolute, bottom: 0, top: 0, left: 0, right: 0 */}
+      <View style={StyleSheet.absoluteFillObject}>
+        {/* ESTRUTURA DA IMAGEM DE FUNDO */}
+        {data.photos.map((photo, index) => (
+          <BackdropPhoto key={photo.id} photo={photo} index={index} scrollX={scrollX} />
+        ))}
+      </View>
+
+      <Animated.FlatList
+        data={data.photos}
+        keyExtractor={(item) => String(item.id)}
+        horizontal
+        // style={{ flexGrow: 0 }} ou alignItems: "center"
+        snapToInterval={imageWidth + spacing}
+        /* snapToInterval => faz com que a rolagem pare exatamente a cada
+        intervalo especificado,nasta caso, o tamanho da imagem + o gap
+        (spacing). */
+        docelerationRate={"fast"}
+        /* docelerationRate => controla a velocidade de desaceleração da
+        nossa rolagem. */
+        contentContainerStyle={{
+          gap: spacing,
+          paddingHorizontal: (width - imageWidth) / 2,
+          alignItems: "center"
+        }}
+        /* contentContainerStyle => aplica estilo no conteúdo internodo
+        nosso FlatList. */
+        renderItem={({ item, index }) => <Photo item={item} index={index} scrollX={scrollX} />}
+        onScroll={onScroll}
+        /* onScroll => função chamada enquanto "rolamos" nossa lista. */
+        scrollEventThrottle={16}
+        /* scrollEventThrottle => define a freqência que o evento onScroll é chamado (60FPS). */
+        showsHorizontalScrollIndicator={false}
+      /* showsHorizontalScrollIndicator => oculta a "barrinha" horizontalda nossa "rolagem". */
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- rotasApp/App.js
+++ rotasApp/App.js
@@ -1,85 +1,43 @@
+// EXEMPLO DE ROTA STACK
+
 import React from "react";
 import { Text, View } from "react-native";
 
 // import styles:
-import { styles } from "./src/style/style";
-
-// import icon
-import AntDesign from "@expo/vector-icons/AntDesign";
-import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { styles } from "../src/style/style";
 
 // import pages:
-import Home from "./src/pages/Home";
-import About from "./src/pages/About";
+import Home from "../src/pages/Home";
+import About from "../src/pages/About";
 
 import { NavigationContainer } from "@react-navigation/native";
+// navigation fornece o componente que vai envolver a navegação
 
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+// "createNativeStackNavigator" importa as funções da Rota Stack
 
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
-/* alterações do RotaDrawer para o RotaTab:
-- apagou o "createDrawerNavigator"
-- acrescentou o "createBottomTabNavigator" para importar as funções da rota Drawer
-- mudou a const "Drawer" para "Tab"
-- dentro do "NavigationContainer" mudou o "Drawer" para "Tab"
-*/
-
-const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+// esse const engloba os imports "navigation" e "createNativeStackNavigator"
 
 export default function App() {
+  // "NavigationContainer" é o pai da navegação
+  // "Stack.Navigator"  gerencia as páginas
+  // "Stack.Screen" define quantas telas existirão, gerencia as telas
+
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          tabBarActiveBackgroundColor: "#fff",
-          tabBarActiveTintColor: "#0015A1",
-          tabBarStyle: {
-            backgroundColor: "#0015A1",
-            height: 50,
-            position: "absolute",
-            bottom: 50,
-            borderRadius: 20,
-            borderTopColor: "transparent",
-            padding: 10,
-            shadowColor: "#0015A1",
-            shadowOffset: { width: 0, height: 4 },
-            shadowOpacity: 0.3,
-            shadowRadius: 4.65,
-            elevation: 8,
-            width: "80%",
-            // left: "10%",
-            // right: "10%",
-            marginHorizontal: "10%",
-          },
-        }}
-      >
-        <Tab.Screen
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
           name="Home"
           component={Home}
-          options={{
-            tabBarIcon: ({ color, size }) => {
-              return <AntDesign name="home" size={24} color={color} />;
-            },
-          }}
+          options={{ headerShown: false }}
         />
-        <Tab.Screen
+        <Stack.Screen
           name="About"
           component={About}
-          options={{
-            tabBarIcon: ({ color, size }) => {
-              return (
-                <MaterialCommunityIcons
-                  name="newspaper-variant-outline"
-                  size={24}
-                  color={color}
-                />
-              );
-            },
-          }}
+          options={{ headerShown: false }}
         />
-      </Tab.Navigator>
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
